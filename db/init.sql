567613a0b511da77c11f2a51cab48c5ac2b365c1
estebanmatias92 <estebanmatias92@gmail.com>
2023-09-23 19:19:34 -0300

    Cambiado selector, datos iniciales fuera del bucle, sentencia SQL afuera, responsabilidades separadas

--- db/init.sql
+++ db/init.sql
@@ -4,23 +4,26 @@ CREATE TABLE IF NOT EXISTS scores (
 );
 
 
-
-DROP TABLE IF EXISTS skins; 
+--DROP TABLE IF EXISTS skins; 
 CREATE TABLE IF NOT EXISTS skins (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     skin_dir TEXT NOT NULL
 );
 
-INSERT INTO skins(skin_dir) 
-VALUES 
-("GameTextures/TextureSkin1.png"),
-("GameTextures/TextureSkin2.png");
 
-DROP TABLE IF EXISTS user;
+--DROP TABLE IF EXISTS user;
 CREATE TABLE IF NOT EXISTS user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     id_skin INTEGER,
     FOREIGN KEY (id_skin) REFERENCES skins(id)
 );
 
+
+/*
+INSERT INTO skins(skin_dir) 
+VALUES 
+("GameTextures/TextureSkin1.png"),
+("GameTextures/TextureSkin2.png");
+
 INSERT INTO user(id_skin) VALUES (1);
+*/
